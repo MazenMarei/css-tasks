f1fb88bbd3d3affa634376de5ddcc83d91cf7242
MazenMarei <moazmazen9199@gmail.com>
2024-08-08 00:09:51 +0300

    navbar and loading screan

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$("ready" , () => {
+        setTimeout(() => {
+            $("#loading").fadeOut(1000);
+            $("main").fadeIn(1000);
+        } , 6000);    
+        $("main").toggleClass("d-none");
+})
